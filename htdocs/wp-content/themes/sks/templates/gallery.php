3185094d20c577d9ecc998aae908dde89f606c86
Kari Savolainen <kari@seravo.fi>
2018-02-25 15:14:28 +0200

    gallery: remove caption

--- htdocs/wp-content/themes/sks/templates/gallery.php
+++ htdocs/wp-content/themes/sks/templates/gallery.php
@@ -40,14 +40,14 @@ if ( post_password_required() ) {
 
 				if( $imgs ): ?>
 
-				        <?php foreach( $imgs as $image ): ?>
-				            <div class="item">
-				                <a href="<?php echo $image['url']; ?>" class="fresco"   data-fresco-group="unique_name">
-				                     <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
-				                </a>
-				                <p><?php echo $image['caption']; ?></p>
-				            </div>
-				        <?php endforeach; ?>
+        <?php foreach( $imgs as $image ): ?>
+            <div class="item">
+                <a href="<?php echo $image['url']; ?>" class="fresco"   data-fresco-group="unique_name">
+                     <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
+                </a>
+                <!-- p><?php echo $image['caption']; ?></p -->
+            </div>
+        <?php endforeach; ?>
 
 				<?php endif; ?>
 			</div>
